e61ad4dd78fa43dc505f7c21619b63a0a0c89221
Student <example@example.com>
2019-11-20 14:58:52 -0800

    fetch functionality added, promise exercise

--- src/main.js
+++ src/main.js
@@ -5,41 +5,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 // API Framework
-
-let request = new XMLHttpRequest();
-let request2 = new XMLHttpRequest();
-const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=XSMRMk633jOJY6SL1FRQjaRTnJZKgQUJcgFqRcNH`;
-const url2 = `https://api.nasa.gov/planetary/apod?api_key=XSMRMk633jOJY6SL1FRQjaRTnJZKgQUJcgFqRcNH&date=2019-11-20`;
-
-
-request.onreadystatechange = function() {
-  if (this.readyState === 4 && this.status === 200) {
-    const response = JSON.parse(this.responseText);
-    getElements(response);
-  }
-}
-
-request.open("GET", url, true);
-request.send();
+fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=XSMRMk633jOJY6SL1FRQjaRTnJZKgQUJcgFqRcNH`)
+.then(function(response) {
+  return response.json();
+})
+.then(function(jsonifiedResponse) {
+  getElements(jsonifiedResponse);
+});
 
 const getElements = function(response) {
   $('#marsPhotos').attr("src", response.photos[0].img_src);
-}
+};
 
+fetch(`https://api.nasa.gov/planetary/apod?api_key=XSMRMk633jOJY6SL1FRQjaRTnJZKgQUJcgFqRcNH&date=2004-12-17`)
+.then(function(response2) {
+  return response2.json();
+})
+.then(function(jsonifiedResponse2){
+  getElements2(jsonifiedResponse2);
+});
 
+const getElements2 = function(response2) {
+  $('#dailyDesc').text(response2.explanation);
+  $('#dailyPhotos').attr('src', response2.url);
+};
 
-request2.onreadystatechange = function() {
-  if (this.readyState === 4 && this.status === 200) {
-    const response = JSON.parse(this.responseText);
-    getElements2(response);
-  }
-}
-
-request2.open("GET", url2, true);
-request2.send();
-
-const getElements2 = function(response) {
-  $('#dailyPhotos').attr('src', response.url);
-  $('#dailyDesc').text(response.explanation);
-
-}
+// Promise.all([getElements, getElements2]);
